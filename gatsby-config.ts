881ee9a71910038e100fabff36a2d6b1f3bb8e3d
Hai Truong <ximbong91023@yahoo.com>
2025-03-07 06:59:21 +0700

    fix build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -25,7 +25,7 @@ const config: GatsbyConfig = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: './src/images/',
+        path: './src/assets/images/',
       },
       __key: 'images',
     },
